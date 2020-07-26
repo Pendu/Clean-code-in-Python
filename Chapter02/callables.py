b6498c2ef83d1020b8f40f5b621c059d4ea4afba
Abhijeet Pendyala <p.abhijeet777@gmail.com>
2020-07-26 13:20:24 +0200

    add explanation of defaultdict

--- Chapter02/callables.py
+++ Chapter02/callables.py
@@ -26,6 +26,12 @@ class CallCount:
     """
 
     def __init__(self):
+        """ default value of int is 0"""
+        """A defaultdict works exactly like a normal dict, but it is
+           initialized with a function (“default factory”) that takes no
+           arguments and provides the default value for a nonexistent key.
+           A defaultdict will never raise a KeyError. Any key that does not
+           exist gets the value returned by the default factory"""
         self._counts = defaultdict(int)
 
     def __call__(self, argument):
