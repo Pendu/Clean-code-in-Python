100fb0b94d85e6523c91393631fd420ca87ec6db
Abhijeet Pendyala <p.abhijeet777@gmail.com>
2020-08-03 05:32:57 +0200

    orthogonal

---
+++ Chapter03/Uop/orthogonal_1.py
@@ -0,0 +1,8 @@
+def calculate_price(base_price: float, tax: float, discount: flaot) -> float:
+    return (base_price*(1*tax) * (1-discount))
+
+def show_price(price: float) -> float:
+    return "$ {0:,.2f}".format(price)
+
+def str_final_price(base_price: float, tax: float, discount: float, fmt_function = str) -> str:
+    return fmt_function(calculate_price(base_price, tax, discount))
