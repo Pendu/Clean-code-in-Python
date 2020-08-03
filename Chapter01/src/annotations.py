9d211da51a15f302d3900e7d0547967012c94d5d
Abhijeet Pendyala <p.abhijeet777@gmail.com>
2020-08-03 13:15:43 +0200

    add annotations

---
+++ Chapter01/src/annotations.py
@@ -0,0 +1,18 @@
+class Point:  # pylint: disable=R0903
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+
+def locate(latitude: float, longitude: float) -> Point:
+    """By mentioning the input variable type and return type for this method,
+       the function is now statically typed, not dynamically typed. Now we can
+       use mypy. check first page of mypy documentation to understand this further"""
+    """Find an object in the map by its coordinates"""
+    return Point(latitude, longitude)
+
+
+class NewPoint:  # pylint: disable=R0903
+    """Example to display its __annotations__ attribute."""
+    lat: float
+    long: float
